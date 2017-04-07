80b1b1ae0aab528a6a1bae51028ee36654fcd0c5
ben <nemewesa@gmail.com>
2017-04-07 14:15:49 +0200

    erstes Chess

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -10,6 +10,35 @@ namespace Chess
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Bitte Zahl eingeben: ");
+
+            int vertical = Convert.ToInt32(Console.ReadLine());
+            int horizontal = vertical;
+            string[,] table = new string[vertical, horizontal];
+             
+            for(int i=0; i<vertical; i++) //A B C D E F G H 
+            {
+                for(int j=0; j<horizontal; j++) //  1 2 3 4 5 6 7 8
+                {
+                    if ((i+j)%2 == 0)
+                    {
+                        table[i, j] = "[ ]";
+                    }
+                    else
+                    {
+                        table[i, j] = "[.]";
+                    }
+                }
+            }
+            for (int i=0; i<horizontal; i++)
+            {
+                for (int j=0; j<vertical; j++)
+                {
+                    Console.Write(table[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
